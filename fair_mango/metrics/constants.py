174e66074b8feac076fb6719e88bd723344a8c41
Amine MAHIDDINE <aminemahiddine.code@gmail.com>
2025-07-23 15:43:14 +0100

    ENH: dictionary merging to handle default values , using module constant

---
+++ fair_mango/metrics/constants.py
@@ -0,0 +1,15 @@
+"""Constants for fairness metrics."""
+
+# Default thresholds for bias detection
+DEFAULT_BIAS_THRESHOLDS = {
+    "demographic_parity_difference": 0.1,
+    "disparate_impact_difference": 0.1,
+    "equal_opportunity_difference": 0.1,
+    "false_positive_rate_difference": 0.1,
+    "equalised_odds_difference": 0.1,
+    "demographic_parity_ratio": 0.8,
+    "disparate_impact_ratio": 0.8,
+    "equal_opportunity_ratio": 0.8,
+    "false_positive_rate_ratio": 0.8,
+    "equalised_odds_ratio": 0.8,
+}
